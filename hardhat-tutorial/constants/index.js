2b5c25c21baeb90be1a3205120153af655a9b4c3
PeterVermeulen01123 <94803849+PeterVermeulen01123@users.noreply.github.com>
2022-03-29 07:55:47 -0400

    adding intial files to repo

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x6414830C44647c4dc240cb41E31a9faF4A65c1dd";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
